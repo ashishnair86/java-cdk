134a07bbed343a83ca97d066d22b5213b65ad1e8
Ashish Nair <ashish882000@gmail.com>
2025-08-17 11:51:13 +0530

    returns VPC name

--- src/main/java/com/myorg/MyVpcStack.java
+++ src/main/java/com/myorg/MyVpcStack.java
@@ -11,6 +11,8 @@ import software.amazon.awscdk.services.ec2.Vpc;
 import software.constructs.Construct;
 
 public class MyVpcStack extends Stack {
+
+    private final Vpc vpc;
     public MyVpcStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -18,8 +20,22 @@ public class MyVpcStack extends Stack {
     public MyVpcStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        Vpc vpc = Vpc.Builder.create(this, "MyJAVAVPC")
-            .ipAddresses(IpAddresses.cidr("192.168.0.0/16"))
+        // Vpc vpc = Vpc.Builder.create(this, "MyJAVAVPC")
+        //     .ipAddresses(IpAddresses.cidr("192.168.0.0/16"))
+        //      .subnetConfiguration(Arrays.asList(
+        //         SubnetConfiguration.builder()
+        //         .name("Application-subnet")
+        //         .subnetType(SubnetType.PUBLIC)
+        //         .cidrMask(24)
+        //         .build(),
+        //         SubnetConfiguration.builder()
+        //         .name("Private-subnet")
+        //         .subnetType(SubnetType.PRIVATE_WITH_EGRESS)
+        //         .cidrMask(24)
+        //         .build()
+           //  ))
+     vpc = Vpc.Builder.create(this, "MyVPC") 
+         .ipAddresses(IpAddresses.cidr("192.168.0.0/16"))
              .subnetConfiguration(Arrays.asList(
                 SubnetConfiguration.builder()
                 .name("Application-subnet")
@@ -31,8 +47,7 @@ public class MyVpcStack extends Stack {
                 .subnetType(SubnetType.PRIVATE_WITH_EGRESS)
                 .cidrMask(24)
                 .build()
-             ))
-
+            ))
 
 
              .maxAzs(2)
@@ -40,5 +55,8 @@ public class MyVpcStack extends Stack {
             .build();
 
 
+    }
+    public Vpc getVpc() {
+        return vpc;
     }
 }
